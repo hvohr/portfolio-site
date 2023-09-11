0dfaa5b3653b973b24b2f21520440128576abe25
Hollis Vohr <hollis.vohr@gmail.com>
2023-09-11 13:10:12 -0600

    feat: add neon name and title to home

---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import './pages.css'
+function Home() {
+  return (
+    <section class="main">
+      <div class='name-container'>
+        <div class="name">Hol</div><div>lis&nbsp;</div><div class='first-last'>Vo</div><div>hr</div>
+      </div>
+      <div class='title-container'>
+        <div class="title">Frontend&nbsp;</div>
+        <div class='title-last'>Developer</div>
+      </div>
+    </section>
+  )
+}
+
+export default Home
