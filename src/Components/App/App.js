9a97c87106bbf0f0497006aa41338e78aa67f51f
Hollis Vohr <hollis.vohr@gmail.com>
2023-09-11 14:25:35 -0600

    feat: add styling for home page and name label

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,19 @@
 import '../App/App.css';
 import Home from '../../Pages/Home'
+import { Routes, Route } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-     <NavLink>
+      <nav>
+        <NavLink to='/'/>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        {/* <Route to='/about' element={<About/>}/>
+        <Route to='/projects' element={<Projects/>}/> */}
+      </Routes>
     </div>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,10 @@
 import './pages.css'
-import '../Components/App/App.css'
 
 function Home() {
   return (
     <section className="main">
       <div className='name-container'>
-        <div className="name">Hol</div><div>lis&nbsp;</div><div class='first-last'>Vo</div><div>hr</div>
+        <div className="name">Hol</div><div>lis&nbsp;</div><div className='first-last'>Vo</div><div>hr</div>
       </div>
       <div className='title-container'>
         <div className="title">Frontend&nbsp;</div>
