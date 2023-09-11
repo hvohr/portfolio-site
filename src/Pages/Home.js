a3dc082e36fe30c832e111bf30f967dc338d8bc0
Hollis Vohr <hollis.vohr@gmail.com>
2023-09-11 15:39:59 -0600

    feat: add swinging lamp and animations

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,14 +2,17 @@ import './pages.css'
 
 function Home() {
   return (
-    <section className="main">
-      <div className='name-container'>
-        <div className="name">Hol</div><div>lis&nbsp;</div><div className='first-last'>Vo</div><div>hr</div>
-      </div>
-      <div className='title-container'>
-        <div className="title">Frontend&nbsp;</div>
-        <div className='title-last'>Developer</div>
-      </div>
+    <section className='home-container'>
+      <section className="main">
+        <div className='name-container'>
+          <div className="name">Hol</div><div>lis&nbsp;</div><div className='first-last'>Vo</div><div>hr</div>
+        </div>
+        <div className='title-container'>
+          <div className="title">Frontend&nbsp;</div>
+          <div className='title-last'>Developer</div>
+        </div>
+      </section>
+      <img class="swingimage" src={require('../Components/images/Contemporary-Hanging-Lamp-PNG-Image.png')}/>
     </section>
   )
 }
