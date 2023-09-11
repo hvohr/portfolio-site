a108505c3a60a8bbe5b11d806c6ca656fd2e0a13
Hollis Vohr <hollis.vohr@gmail.com>
2023-09-11 17:05:03 -0600

    fix: styling of hanging lamp for responsiveness

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import '../App/App.css';
 import Home from '../../Pages/Home'
 import About from '../../Pages/About'
 import Projects from '../../Pages/Projects'
+import Contact from '../../Pages/Contact'
 
 import { Routes, Route } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
@@ -13,11 +14,14 @@ function App() {
         <NavLink className='home-button' to='/'>HV</NavLink>
         <NavLink className='about-button' to='/about'>About</NavLink>
         <NavLink className='projects-button' to='/projects'>Projects</NavLink>
+        <NavLink className='contact-button' to='/contact'>Contact</NavLink>
       </nav>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About/>}/>
         <Route path='/projects' element={<Projects/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+
       </Routes>
     </div>
   );
