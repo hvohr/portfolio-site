fa934ca7bad415def99757e7e2bcd9fc2332002b
Hollis Vohr <hollis.vohr@gmail.com>
2023-09-12 12:06:58 -0600

    feat: add beginning of christmas light structure

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -6,7 +6,7 @@ function About() {
       <section className='about-container'>
         <p className='about-me'>
           Welcome! My name is Hollis and I live in Denver, Colorado.
-          My coding journey began with the desire to create beautiful, seamlessly functioning, user-friendly web experiences. With expertise in HTML/CSS, JavaScript, React and other frontend frameworks, I specialize in turning ideas into realities. User experience is at the core of my work.
+          My coding journey began with the desire to create beautiful, seamlessly functioning, user-friendly web experiences. I have expertise in HTML/CSS, JavaScript, React and other frontend frameworks and user experience is at the core of my work.
         </p>
         <p className='about-me'>
           As a lifelong learner, I'm committed to staying at the forefront of development trends and best practices. The digital world is constantly changing, and I am stoked at the opportunity to continuously expand my skill set. Working closely with designers and back-end developers, I work to transform concepts into responsive, pixel-perfect web solutions that leave a lasting impression.
