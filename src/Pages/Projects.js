86f3e347f390432fbd6519b3813239fc93039fbe
Hollis Vohr <hollis.vohr@gmail.com>
2023-09-13 13:48:39 -0600

    feat: add other project contents for functional modals

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,6 +1,11 @@
 import '../Components/App/App.css';
 
 function Projects() {
+
+  function handleModal() {
+
+  }
+
   return (
     <section>
       <div className='bare-minimum-container'>
@@ -10,19 +15,27 @@ function Projects() {
           <p className='project-description'>Welcome to Bare Minimum! Feeling discouraged about completing large scale tasks and projects? Well, our application is the one for you! Bare Minimum is your go to organization website that breaks down tasks into small simple ones. A user can choose from any of the categories and be presented with a task that is smaller and more easily manageable! If the user feels like the task is something they can accomplish, we have the option to save this task to a personal list. This way, a user can view all their saved tasks and mark them when they are complete! A user can also decide to delete a task from that list when they no longer wish to complete it. Our app is fully functioning on both your computer or your phone, making it easy to complete these tasks on the go! We hope you enjoy Bare Minimum!</p>
           <p>Technologies Used: ExpressJS, Javascript, ReactJS, HTML/CSS, Cypress, Webpack, FETCH API's, Knex, CORS</p>
           <a className='bare-mimimum-source' href="https://github.com/hvohr/bare-minimum" target="_blank" rel="noopener noreferrer"> View Source Code</a>
+          <a className='clear-skies-source' href="https://bare-minimum.vercel.app/" target="_blank" rel="noopener noreferrer">View Deployed Link</a>
         </section>
       </div>
       <div className='clear-skies-container'>
         <h3>Clear Skies</h3>
         <button>View More</button>
-
-        <a className='clear-skies-source' href="https://www.buymeacoffee.com/hollisvohr" target="_blank" rel="noopener noreferrer">View Source Code</a>
+        <section className='modal-container'>
+          <p className='project-description'>Welcome to ClearSkies! A weather application that lets a user know a cities current and daily weather. This app also lets you search cities and view real time weather information! It is only available to people in the US and to get the full experience, make sure you have location services turned on - this allows ClearSkies to obtain your current weather right outside your front door. I hope you enjoy ClearSkies.</p>
+          <p>Technologies Used: Javascript, ReactJS, HTML/CSS, Cypress, Webpack, FETCH API's</p>
+          <a className='clear-skies-source' href="https://github.com/hvohr/clearskies" target="_blank" rel="noopener noreferrer">View Source Code</a>
+          <a className='clear-skies-source' href="https://clearskies.vercel.app/" target="_blank" rel="noopener noreferrer">View Deployed Link</a>
+        </section>
       </div>
-      <div className='clear-skies-container'>
+      <div className='porfolio-container'>
         <h3>Portfolio</h3>
         <button>View More</button>
-
-        <a className='porfolio-source' href="https://www.buymeacoffee.com/hollisvohr" target="_blank" rel="noopener noreferrer">View Source Code</a>
+        <section className='modal-container'>
+          <p className='project-description'>Welcome to my personal porfolio. You are currently viewing the deployed product, so enjoy and don't hesitate to reach out(include link to contact lmao) to me!</p>
+          <p>Technologies Used: React, Javascript, ReactJS, HTML/CSS</p>
+          <a className='porfolio-source' href="https://github.com/hvohr/portfolio-site" target="_blank" rel="noopener noreferrer">View Source Code</a>
+        </section>
       </div>
     </section>
   )
